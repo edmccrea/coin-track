654aed70d11a60491a52b1b7dc90e5714ba4b4d1
edmccrea <ed-mccrea@hotmail.co.uk>
2022-03-10 16:04:55 +0100

    currency format working on safari

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -1,5 +1,5 @@
 export const currencyFormat = (x) => {
-  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  return x.toLocaleString('en');
 };
 
 export const colorCheck = (num, theme) => {
